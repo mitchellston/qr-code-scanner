be7e5660773301637d5ca4de8306aee59570bc25
mitchellston <m.steenwijk@live.nl>
2023-07-08 01:24:31 +0200

    Revert "Finalize for v2.5.0 Release"

--- packages/test/vite.config.ts
+++ packages/test/vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig((config) => ({
+export default defineConfig({
   plugins: [react()],
-  base: config.mode == "development" ? "" : "/qr-code-scanner",
-}));
+})
